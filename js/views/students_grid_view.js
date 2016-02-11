530677d245e7d70af2193e7dd21a953ec3c05b63
unknown <a557902@INDA202755.intl.intlroot.fid-intl.com>
2016-02-11 12:05:56 +0530

    #tablemodify

--- js/views/students_grid_view.js
+++ js/views/students_grid_view.js
@@ -9,21 +9,17 @@ define(['jquery', 'backbone', 'underscore', 'views/grid_child', 'text!../../temp
 		this.listenTo(this.collection, 'add', this.renderChildView);
 	},
 
-	renderChildView: function() {
-		debugger
-		_.each(this.collection.models, function (model) { 
-	    	debugger
-	    //Set the View 
+	renderChildView: function(model) {
+	    //Set the View */
 	    var studRecHtml = new StudentsGridChildView({model:model}).render().el;
 	    this.$el.find('tbody').append(studRecHtml);
-	}, this);
+
 	},
 
 	//Render View 
 	render: function () {  
 	    $(this.el).html(this.template);
 	   /* _.each(this.collection.models, function (model) { 
-	    	debugger
 	    //Set the View 
 	    var studRecHtml = new StudentsGridChildView({model:model}).render().el;
 	    this.$el.find('tbody').append(studRecHtml);
