76c3dbcd24d4df9c41f488a2f96ddb7da4dddad9
unknown <a557902@INDA202755.intl.intlroot.fid-intl.com>
2016-02-11 14:30:55 +0530

    #delete_functionality

--- js/views/grid_child.js
+++ js/views/grid_child.js
@@ -3,10 +3,24 @@ define(['jquery', 'backbone', 'underscore', 'text!../../templates/studentsGridDa
 	var StudentsGridChildView = Backbone.View.extend({
     tagName : 'tr',
     template: _.template(studentsGridDataTemplate,{}),
+    events: {
+    	'click #deleteStudentBtn': 'deleteStudentBtnClicked'
+    },
+    initialize: function(){
+    	this.model.bind('remove', this.modelChanged, this)
+    },
+
     render : function() {
-    	debugger
         this.$el.html(this.template(this.model.toJSON()));
         return this;
+    },
+
+    deleteStudentBtnClicked: function() {
+    	this.model.destroy();
+    },
+    
+    modelChanged: function(){
+    	this.remove();
     }
 });
 	 return StudentsGridChildView;
